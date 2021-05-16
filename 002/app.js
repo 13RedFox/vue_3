c66ed06b2396eb0330817192d377e5c123cf8acc
13RedFox <sergei181989@gmail.com>
2021-05-16 13:08:37 +0300

    fix: edit 002 project

--- 002/app.js
+++ 002/app.js
@@ -1,33 +1,32 @@
 const App = {
-	data() {
-		return {
-			title: 'Список заметок',
-			placeholderString: 'Введите название заметки, но не длиннее 40 символов.',
-			inputValue: '',
-			notes: [],
-		}
-	},
-	methods: {
-		addNewNote() {
-			if (this.inputValue !== '') {
-				this.notes.push(this.inputValue)
-				this.inputValue = ''
-			}
-		},
-		toUpperCase(item) {
-			return item.toUpperCase()
-		},
-		removeNote(idx) {
-			this.notes.splice(idx, 1)
-		}
-	},
-	watch: {
-		inputValue(value) {
-			if (value.length > 40) {
-				this.inputValue = ''
-			}
-			console.log(value)
-		}
-	}
+  data() {
+    return {
+      title: 'Список заметок',
+      placeholderString: 'Введите название заметки',
+      inputValue: '',
+      notes: [],
+    }
+  },
+  methods: {
+    addNewNotes() {
+      if (this.inputValue !== '') {
+        this.notes.push(this.inputValue);
+        this.inputValue = '';
+      }
+    },
+    removeNewNotes(idx) {
+      this.notes.splice(idx, 1);
+    }
+  },
+  computed: {
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 30) {
+        this.inputValue = '';
+      }
+    }
+  },
 }
+
 Vue.createApp(App).mount('#app');
