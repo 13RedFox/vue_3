06de466fbbd5fa19566be0bb76b4597a6afc572a
13RedFox <sergei181989@gmail.com>
2021-05-17 23:20:25 +0300

    feat: added 06-vue-form project

---
+++ 06-vue-forms/src/AppInput.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="form-control" :class="{invalid: error}">
+		<label :for="id">{{ label }}</label>
+		<input
+				type="text"
+				:id="name"
+				:placeholder="placeholder"
+				:value="modelValue"
+				@input="change"
+		>
+		<small v-if="error">{{ error }}</small>
+	</div>
+</template>
+
+<script>
+	export default {
+		emits: ['update:value'],
+		props: {
+			value: String,
+			placeholder: String,
+			label: String,
+			error: String,
+		},
+		data() {
+			return {
+				id: 'input-' + Math.random()
+			}
+		},
+		methods: {
+			change(event) {
+this.$emit('update:value', event.target.value)
+			}
+		},
+	}
+</script>
+
+<style scoped>
+
+</style>
